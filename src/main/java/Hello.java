0dd4fdfd55c8717966884354d6b712b3ab697cff
Alt_Scape <tubaltkn@gmail.com>
2022-01-03 23:33:45 +0000

    update more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
 
         System.out.print("Hello");
         System.out.println("developing hello feature");
+        System.out.println("local repo is already published to github");
+        System.out.println("doing more work");
     }
 }
